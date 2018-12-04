2c4ea4cc9bb60829551e22f2a3ce1ffad4522d52
minhtungbuichau <1512651@student.hcmus.edu.vn>
2018-12-05 00:55:47 +0700

    handle popup when click something

---
+++ src/action/postItemAction.js
@@ -0,0 +1,20 @@
+import * as ActionTypes from './../constant/ActionTypes'
+
+export const onHeartReact = () => {
+    return {
+        type: ActionTypes.HEART_REACT
+    }
+};
+
+export const onCommentReact = () => {
+    return {
+        type: ActionTypes.COMMENT_REACT
+    }
+};
+
+export const onShareReact = () => {
+    return {
+        type: ActionTypes.SHARE_REACT
+    }
+};
+
--- src/constant/ActionTypes.js
+++ src/constant/ActionTypes.js
@@ -3,3 +3,6 @@ export const VIEW_FOLLOWING = 'VIEW_FOLLOWING';
 export const VIEW_FOLLOWER = 'VIEW_FOLLOWER';
 export const WRITE_POST = 'WRITE_POST';
 export const EDIT_USER_INFO  = 'EDIT_USER_INFO';
+export const HEART_REACT = 'HEART_REACT';
+export const COMMENT_REACT = 'COMMENT_REACT';
+export const SHARE_REACT = 'SHARE_REACT';
