300034d3f21f656e1ede210edc3efca31e15a92d
minhtungbuichau <1512651@student.hcmus.edu.vn>
2018-12-22 20:33:29 +0700

    login form with secret key

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Home from './components/Home';
-
+import Login from './components/Login'
 class App extends Component {
   render() { 
 
     return (
       <div className="App">
-        <Home/>
+        {/* <Home/> */}
+        <Login/>
       </div>
     );
   }
---
+++ src/action/loginAction.js
@@ -0,0 +1,7 @@
+import * as ActionTypes from './../constant/ActionTypes'
+
+export const login = () => {
+    return {
+        type: ActionTypes.LOGIN
+    }
+};
